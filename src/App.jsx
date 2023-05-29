feb9235354eaa6d3abb3b85b67dc21b0e99ad0c8
Gonza <gonzagramaglia@gmail.com>
2023-05-29 14:40:14 -0300

    Branding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { FaGithub } from 'react-icons/fa';
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <>
+            <nav>
+                <a 
+                    href="https://www.gonza.gr/" 
+                    target="_blank"
+                >
+                    <img src="/logo.png" alt="logo" className='logo' /> 
+                </a>
+                <a 
+                    href="https://github.com/gonzagramaglia/social" 
+                    className='github-logo-container'
+                    target="_blank"
+                >
+                    <FaGithub size={23} style={{ color: '#a9a6c3' }} />
+                </a>
+            </nav>
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        </>
     )
 }
 
