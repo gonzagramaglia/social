846035fabf6036cfd17849b930684e0c947d3ce8
Gonza <gonzagramaglia@gmail.com>
2023-05-29 17:06:27 -0300

    Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
             </nav>
             <Box>
                 {/* <Navbar /> */}
-                <Stack direction="row" spacing={2} justifyContent="space-between" >
+                <Stack direction="row" spacing={ {xs:0, sm:2} } justifyContent="space-between" >
                     <Sidebar />
                     <Feed />
                     <Rightbar />
