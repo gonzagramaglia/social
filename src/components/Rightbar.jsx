28c099c22edb6be7abb70871afd0f544a08d231f
Gonza <gonzagramaglia@gmail.com>
2023-05-29 22:49:38 -0300

    Online Friends

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -7,45 +7,56 @@ const Rightbar = () => {
             flex={2} 
             p={2} 
             sx={{ display: { xs: "none", sm: "block" } }}
+            minWidth={235}
         >
-            <Box position="fixed" width={300}>
+            <Box position="fixed" width={250}>
                 <Typography variant="h6" fontWeight={100}>
                     Online Friends
                 </Typography>
                 <AvatarGroup max={7}>
                     <Avatar
-                    alt="Remy Sharp"
-                    src="https://material-ui.com/static/images/avatar/1.jpg"
+                        alt="Shanks"
+                        src="/one-piece/shanks-profile.webp"
                     />
                     <Avatar
-                    alt="Travis Howard"
-                    src="https://material-ui.com/static/images/avatar/2.jpg"
+                        alt="Rayleigh"
+                        src="/one-piece/rayleigh-profile.webp"
                     />
                     <Avatar
-                    alt="Cindy Baker"
-                    src="https://material-ui.com/static/images/avatar/3.jpg"
-                    />
-                    <Avatar alt="Agnes Walker" src="" />
-                    <Avatar
-                    alt="Trevor Henderson"
-                    src="https://material-ui.com/static/images/avatar/6.jpg"
+                        alt="Hancock"
+                        src="/one-piece/hancock-profile.jpeg"
                     />
                     <Avatar
-                    alt="Trevor Henderson"
-                    src="https://material-ui.com/static/images/avatar/7.jpg"
+                        alt="Sabo"
+                        src="/one-piece/sabo-profile.avif"
+                    />
+                    <Avatar 
+                        alt="Koby" 
+                        src="/one-piece/koby-profile.jpeg" 
                     />
                     <Avatar
-                    alt="Trevor Henderson"
-                    src="https://material-ui.com/static/images/avatar/8.jpg"
+                        alt="Vivi"
+                        src="/one-piece/vivi-profile.avif"
                     />
-                    <Avatar
-                    alt="Trevor Henderson"
-                    src="https://material-ui.com/static/images/avatar/7.jpg"
-                    />
-                    <Avatar
-                    alt="Trevor Henderson"
-                    src="https://material-ui.com/static/images/avatar/8.jpg"
-                    />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
+                    <Avatar alt="" src="" />
                 </AvatarGroup>
                 <Typography variant="h6" fontWeight={100} mt={2}>
                     Latest Conversations
